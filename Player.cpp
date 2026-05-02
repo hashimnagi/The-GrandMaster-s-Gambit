1011314760ce9de9c1dd4c031acb875e63c3b1a9
Ali-hassan389 <f250644@cfd.nu.edu.pk>
2026-05-02 23:43:14 +0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 //Parameterized Constructor
-Player::Player(string n , Color c) : Name(n) , color(c) {};
+Player::Player(string n, Color c) : Name(n), color(c) {};
 
 //Getter Functions
-string Player::GetName( )const {
+string Player::GetName()const {
 	return Name;
 }
-Color Player::GetColor( )const {
+Color Player::GetColor()const {
 	return color;
 }
 
-int Player::GetCaptured( )const {
+int Player::GetCaptured()const {
 	return captured;
 }
 
 // Incrementung captured
-void Player::IncreaseCaptured( ) {
+void Player::IncreaseCaptured() {
 	captured++;
 }
 
 //Display Function
-void Player::DisplayInfo( )const {
+void Player::DisplayInfo()const {
 	cout << Name << "|";
-	if ( color == Color::WHITE )
+	if (color == Color::WHITE)
 		cout << "WHITE";
 	else
 		cout << "BLACK";
