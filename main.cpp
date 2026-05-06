5d8026c2271095f11422ee09b0943dc3733a3438
hashimnagi <hashimnagi77@gmail.com>
2026-05-06 22:43:28 +0500

    Update player name input method and add pause
    Replaced cin with getline for player name input and added system pause.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 // main.cpp
 #include "Game.h"
 #include <iostream>
+#include<string>
 using namespace std;
 
 int main( ) {
     string name1 , name2;
 
     cout << "Enter White player's name: ";
-    cin >> name1;
+    getline(cin,name1);
     cout << "Enter Black player's name: ";
-    cin >> name2;
+    getline(cin,name2);
 
     Game chess(name1 , name2);
     chess.startGUI( );
 
+    system("pause");
     return 0;
 }
